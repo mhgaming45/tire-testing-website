9c9b4392e440abdaf153c84df1bd3e380c4348a5
mhgaming45 <marotimdpbn@gmail.com>
2026-08-12 21:15:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // MHGAMING Tier Website API
 // If this repo is hosted on GitHub Pages, set this to your Wispbyte public URL.
 // Example: https://your-wispbyte-domain.example
-window.MHGAMING_API = '';
+window.MHGAMING_API = 'https://api-mhgaming.wisp.uno';
